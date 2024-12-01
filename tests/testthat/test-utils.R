6c6465292e9ad22134c5a71cb6284ffc13a8279b
Ben Raymond <ben@untan.gl>
2024-12-01 16:31:58 +1100

    fix test

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,7 +1,7 @@
 context("internal utilities")
 
 test_that("various utils work", {
-    expect_null(prc(c()))
+    expect_equal(prc(c()), NA_character_)
     expect_equal(prc(1), "1%")
     expect_true(is.na(prc(NA)))
     expect_equal(prc(c(1, NA)), c("1%", NA_character_))
